5c0b5e7d5f806530c4869d995ac1c48b517d0ab5
lorenzja <112662664+lorenzja@users.noreply.github.com>
2024-09-20 10:28:27 -0400

    Add files via upload

--- Lab_1.R
+++ Lab_1.R
@@ -1,11 +1,16 @@
 #Lab 1 Exercise 1
 
 EPI_data <- read.csv("C:/Users/Jake Lorenzo/Desktop/Data Analytics/Lab 1/epi2024results06022024.csv")
+
 View(EPI_data)
+
 attach(EPI_data)
+
 EPI.new
+
 tf <- is.na(EPI.new)
 E <- EPI.new[!tf]
+
 summary(EPI.new)
 fivenum(EPI.new,na.rm=TRUE)
 stem(EPI.new)
@@ -38,6 +43,7 @@ qqline(EPI.new)
 APO.new <- EPI_data$APO.new
 tf_APO <- is.na(APO.new)
 APO_cleaned_data <- APO.new[!tf_APO]
+
 summary(APO.new)
 fivenum(APO.new, na.rm = TRUE)
 
@@ -47,9 +53,9 @@ lines(density(APO.new, na.rm = TRUE, bw = 1))
 rug(APO.new)
 
 boxplot(APO.new, EPI.new)
-x_APO <- seq(min(APO.new, na.rm=TRUE), max(APO.new, na.rm=TRUE), 1)
-q_APO <- dnorm(x_APO, mean = mean(APO.new, na.rm=TRUE), sd = sd(APO.new, na.rm=TRUE))
-lines(x_APO, q_APO)
+min_max_APO <- seq(min(APO.new, na.rm=TRUE), max(APO.new, na.rm=TRUE), 1)
+density_APO <- dnorm(min_max_APO, mean = mean(APO.new, na.rm=TRUE), sd = sd(APO.new, na.rm=TRUE))
+lines(min_max_APO, density_APO)
 
 plot(ecdf(APO.new), do.points=FALSE, verticals=TRUE)
 qqnorm(APO.new); qqline(APO.new)
@@ -71,9 +77,9 @@ rug(WRS.new)
 
 boxplot(WRS.new, EPI.new)
 
-x_WRS <- seq(min(WRS.new, na.rm=TRUE), max(WRS.new, na.rm=TRUE), 1)
-q_WRS <- dnorm(x_WRS, mean = mean(WRS.new, na.rm=TRUE), sd = sd(WRS.new, na.rm=TRUE))
-lines(x_WRS, q_WRS)
+min_max_WRS <- seq(min(WRS.new, na.rm=TRUE), max(WRS.new, na.rm=TRUE), 1)
+density_WRS <- dnorm(min_max_WRS, mean = mean(WRS.new, na.rm=TRUE), sd = sd(WRS.new, na.rm=TRUE))
+lines(min_max_WRS, density_WRS)
 
 plot(ecdf(WRS.new), do.points=FALSE, verticals=TRUE)
 qqnorm(WRS.new); qqline(WRS.new)
